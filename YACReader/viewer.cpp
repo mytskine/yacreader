7bca59d619333088de6a200b664bb5749f576d4d
François Gannaz <francois.gannaz@silecs.info>
2019-03-22 23:23:41 +0100

    faster basic scroll (arrows)

--- YACReader/viewer.cpp
+++ YACReader/viewer.cpp
@@ -218,6 +218,7 @@ void Viewer::prepareForOpening()
     // render->update();
 
     verticalScrollBar()->setSliderPosition(verticalScrollBar()->minimum());
+    verticalScrollBar()->setSingleStep(100);
 
     if (Configuration::getConfiguration().getShowInformation() && !information) {
         QTimer* timer = new QTimer();
